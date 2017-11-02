6e3aa009c2068f3efceb6421c9e6f2aa96fcf64b
lucascruceru <lucascruceru@gmail.com>
2017-11-02 11:30:33 +0200

    Added some stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
-var xhttp = new XMLHttpRequest();
-xhttp.open("GET", "info.txt", true);
-xhttp.send();
-xhttp.onreadystatechange = function () {
-    console.log(this.readyState);
-    console.log(this.status);
-    console.log(this.responseText);
+function load() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "assets/info.txt", true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if(this.readyState === 4 && this.status === 200) {
+            console.log(this.statusText);
+            console.log(this.readyState);
+            console.log(this.status);
+            console.log(this.responseText);
+            document.getElementById("xml").innerHTML =  this.responseText;
+        }
+    }
 }
